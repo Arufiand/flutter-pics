56d52fa8f28ec1f374ce8b7cde3e5f39c2087530
arufiand <alfiandannyarmanta@gmail.com>
2024-09-03 21:36:05 +0700

    feat: adding fetchImage functions
    Author: @arufiand

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   @override
@@ -8,22 +11,34 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  int counter = 1;
+  int counter = 0;
+  List<ImageModel> images = [];
 
+  void fetchImage() async {
+    counter++;
+    var response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/${counter}')
+            as String);
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
       body: Text('${counter}'),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          }),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: fetchImage,
+        child: Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text('Let See some images!'),
       ),
     ));
   }
 }
+
+Future<String> get(String url) {}
