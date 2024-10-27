0d9dd2e3d02806096fbfe271510c9dd7cd5f18f2
arufiand <alfiandannyarmanta@gmail.com>
2024-10-27 19:23:01 +0700

    feat: ngulang
    Author: @arufiand

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
 
 void main() {
-  runApp(App());
+  var app = MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
+  );
+
+  runApp(app);
 }
