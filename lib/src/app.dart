79cb6cf89807357109d878afaed17c2e439c0f99
arufiand <alfiandannyarmanta@gmail.com>
2024-10-27 20:56:54 +0700

    feat: Creating Statefull widget Author: @arufiand

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () {
+            setState(() {
+              counter += 1;
+            });
             print('Hi there! dari app.dart');
           },
         ),
