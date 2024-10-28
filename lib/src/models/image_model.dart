0298f98c242c18f1e2654fc2a32ed6b2ebcffc24
arufiand <alfiandannyarmanta@gmail.com>
2024-10-28 11:57:51 +0700

    feat: Creating image_model.dart Author: @arufiand

---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,21 @@
+import '';
+
+class ImageModel {
+  int id;
+  String url;
+  String title;
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
+
+// another approach
+// ImageModel.fromJson(Map<String, dynamic> parsedJson)
+//     : id = parsedJson['id'],
+//       url = parsedJson['url'],
+//       title = parsedJson['title'];
+}
